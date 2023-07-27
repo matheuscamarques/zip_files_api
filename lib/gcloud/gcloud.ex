82a8b0a061f980236072b6b12836338f3635b5b1
Matheus C Marques <matheuscamarques@gmail.com>
2023-07-27 11:37:37 -0300

    fix: remove comments

--- lib/gcloud/gcloud.ex
+++ lib/gcloud/gcloud.ex
@@ -24,27 +24,35 @@ defmodule Gcloud do
     {:ok, conn} = NewStorageConnection.method(token)
     {:ok, bucket} = GetStorageBucket.method(conn)
 
-    {:ok,
-     %{
-       selfLink: self_link
-     }} = UploadFile.method(conn, bucket, file_path, rename_to)
-
-    {:ok, self_link}
+    UploadFile.method(conn, bucket, file_path, rename_to)
   end
 
-  def resolve_file(file_src) do
+  def download_file(file_src, target_file_path) do
     {:ok, token} = CreateCloudToken.method()
     {:ok, conn} = NewStorageConnection.method(token)
     {:ok, bucket} = GetStorageBucket.method(conn)
     file_src_prepared = URI.encode(file_src, &URI.char_unreserved?/1)
-    {:ok, %{mediaLink: media_link}} =GoogleApi.Storage.V1.Api.Objects.storage_objects_get(conn, bucket.id, file_src_prepared)
-    # make a request to download with media link
-    download_file(token.token,media_link)
+
+    {:ok, %{mediaLink: media_link}} =
+      GoogleApi.Storage.V1.Api.Objects.storage_objects_get(conn, bucket.id, file_src_prepared)
+
+    # Make a request to download with media link
+    binary_data = http_download_file(token.token, media_link)
+    # expand path
+    target_file_path = Path.expand(target_file_path)
+    # Write the downloaded binary data to the target file
+    case File.write(target_file_path, binary_data) do
+      :ok ->
+        {:ok, target_file_path}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
-  defp download_file(token, media_link) do
+  defp http_download_file(token, media_link) do
     headers = [{"Authorization", "Bearer #{token}"}]
-    Tesla.get(media_link, headers: headers, streaming: true)
+    {:ok, response} = Tesla.get(media_link, headers: headers, streaming: true)
+    response.body
   end
-
 end
