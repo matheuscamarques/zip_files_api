465cf88b11e54cb4899cf2abb86bdd98683a6277
Matheus C Marques <matheuscamarques@gmail.com>
2023-08-01 11:44:01 -0300

    fix: reafactor domain test

--- test/zip_files_api/domain_test.exs
+++ test/zip_files_api/domain_test.exs
@@ -2,9 +2,13 @@ defmodule ZipFilesApi.DomainTest do
   use ZipFilesApiWeb.ConnCase, async: false
 
   test "Receive Payload and process PDF and merge true" do
-    file_path = Application.app_dir(:zip_files_api, "priv") |> Path.join("test/test_image.png")
-    {:ok, _file_1} = Gcloud.upload_file(file_path, "file1.png")
-    {:ok, _file_2} = Gcloud.upload_file(file_path, "file2.png")
+    file_path_1 = Application.app_dir(:zip_files_api, "priv") |> Path.join("test/test_image.png")
+    {:ok, _file_1} = Gcloud.upload_file(file_path_1, "file1.png")
+
+    file_path_2 =
+      Application.app_dir(:zip_files_api, "priv") |> Path.join("test/test_image_2.jpg")
+
+    {:ok, _file_2} = Gcloud.upload_file(file_path_2, "file2.png")
 
     payload = %{
       files: [
@@ -26,6 +30,81 @@ defmodule ZipFilesApi.DomainTest do
       filename_pattern: "PERSON_NAME-TIMESTAMP-DOCUMENT_KEY.EXTENSION"
     }
 
-    assert {:ok, %GoogleApi.Storage.V1.Model.Object{}} = ZipFilesApi.make_zip_file(payload)
+    assert {:ok, %GoogleApi.Storage.V1.Model.Object{}} =
+             ZipFilesApi.make_zip_file(payload) |> IO.inspect()
+  end
+
+  test "Receive Payload and process PDF and merge true and multiple persons" do
+    file_path_1 = Application.app_dir(:zip_files_api, "priv") |> Path.join("test/test_image.png")
+    {:ok, _file_1} = Gcloud.upload_file(file_path_1, "file1.png")
+
+    file_path_2 =
+      Application.app_dir(:zip_files_api, "priv") |> Path.join("test/test_image_2.jpg")
+
+    {:ok, _file_2} = Gcloud.upload_file(file_path_2, "file2.png")
+
+    payload = %{
+      files: [
+        %{
+          document_key: "RG",
+          src: "file1.png",
+          person_name: "Matheus Marques"
+        },
+        %{
+          document_key: "CPF",
+          src: "file2.png",
+          person_name: "Matheus Marques"
+        },
+        %{
+          document_key: "CPF",
+          src: "file2.png",
+          person_name: "Igor Aguiar"
+        },
+        %{
+          document_key: "RG",
+          src: "file1.png",
+          person_name: "Igor Aguiar"
+        }
+      ],
+      format: "PDF",
+      merge: true,
+      filename_pattern: "PERSON_NAME-TIMESTAMP-DOCUMENT_KEY.EXTENSION"
+    }
+
+    assert {:ok, %GoogleApi.Storage.V1.Model.Object{}} =
+             ZipFilesApi.make_zip_file(payload) |> IO.inspect()
+  end
+
+  test "Receive Payload and process PDF and merge false" do
+    file_path_1 = Application.app_dir(:zip_files_api, "priv") |> Path.join("test/test_image.png")
+    {:ok, _file_1} = Gcloud.upload_file(file_path_1, "file1.png")
+
+    file_path_2 =
+      Application.app_dir(:zip_files_api, "priv") |> Path.join("test/test_image_2.jpg")
+
+    {:ok, _file_2} = Gcloud.upload_file(file_path_2, "file2.png")
+
+    payload = %{
+      files: [
+        %{
+          document_key: "RG",
+          src: "file1.png",
+          person_name: "Matheus Marques",
+          timestamp: DateTime.utc_now() |> DateTime.to_unix() |> Integer.to_string()
+        },
+        %{
+          document_key: "CPF",
+          src: "file2.png",
+          person_name: "Matheus Marques",
+          timestamp: DateTime.utc_now() |> DateTime.to_unix() |> Integer.to_string()
+        }
+      ],
+      format: "PDF",
+      merge: false,
+      filename_pattern: "PERSON_NAME-TIMESTAMP-DOCUMENT_KEY.EXTENSION"
+    }
+
+    assert {:ok, %GoogleApi.Storage.V1.Model.Object{}} =
+             ZipFilesApi.make_zip_file(payload) |> IO.inspect()
   end
 end
